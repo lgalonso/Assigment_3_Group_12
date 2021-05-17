fc6d69324b4bb185e1a1638f768916d303a0e336
Lucas G. Alonso <56822984+lgalonso@users.noreply.github.com>
2021-05-17 10:45:30 +0200

    Initial commit

---
+++ app/src/main/java/se/mau/group12/assigment3/MainActivity.java
@@ -0,0 +1,14 @@
+package se.mau.group12.assigment3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
