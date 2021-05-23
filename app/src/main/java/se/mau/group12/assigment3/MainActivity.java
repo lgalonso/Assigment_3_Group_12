644abf460cb7802b08b2529d05b06e6765f5d151
Léane Gasnot <leane.gasnot@edu.ece.fr>
2021-05-23 18:11:11 +0200

    design and structure v3

--- app/src/main/java/se/mau/group12/assigment3/MainActivity.java
+++ app/src/main/java/se/mau/group12/assigment3/MainActivity.java
@@ -4,15 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signInButton, createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this,HomeActivity.class);
-        startActivity(intent);
+        signInButton = findViewById(R.id.button_Sign_in);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        createAccountButton = findViewById(R.id.button_create_account);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
