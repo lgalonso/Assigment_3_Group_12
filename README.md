b20628d09ac866efabf8d9f6ae852798b30558a7
Mathias <mathias.sericola@gmail.com>
2021-05-24 14:18:47 +0200

    Youtube Player

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Assigment_3_Group_12
-Repository for group Assignment 3 in Android Development Course in Malmö University.
+Repository for group Assignment 3   in Android Development Course in Malmö University.
--- app/src/main/java/se/mau/group12/assigment3/MainActivity.java
+++ app/src/main/java/se/mau/group12/assigment3/MainActivity.java
@@ -3,12 +3,38 @@ package se.mau.group12.assigment3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.CalendarContract;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+//public class MainActivity extends AppCompatActivity {
+public class MainActivity extends YouTubeBaseActivity {
+
+    private YouTubePlayerView youTubePlayerView;
+    private YouTubePlayer.OnInitializedListener onInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener(){
+
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("MCf7EWLVWO8",50000);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+            }
+        };
+
+        youTubePlayerView.initialize("AIzaSyAYdEGnQOYZHdlrn4rnx71AVqaC1m_6mHg",onInitializedListener);
+
     }
 }
