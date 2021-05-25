588bc8bc34c4388c637bad78a588e0f6685cabdf
Léane Gasnot <leane.gasnot@edu.ece.fr>
2021-05-25 16:07:26 +0200

    design home page and calendar

--- app/src/main/java/se/mau/group12/assigment3/ui/home/HomeFragment.java
+++ app/src/main/java/se/mau/group12/assigment3/ui/home/HomeFragment.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import se.mau.group12.assigment3.R;
@@ -17,17 +17,24 @@ import se.mau.group12.assigment3.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private TextView textViewtitle;
+    private Button btnNext;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+
+        textViewtitle = root.findViewById(R.id.textViewTitleHome);
+        btnNext = root.findViewById(R.id.buttonNextHome);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                //OPEN DETAILSSESSIONACTIVITY WITH GOOD SESSION ( USE DATABASE)
+
             }
         });
         return root;
