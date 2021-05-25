16c730c643178f140527d8dcabc9ed96d23d8fb9
Lucas G. Alonso <56822984+lgalonso@users.noreply.github.com>
2021-05-25 12:04:27 +0200

    getters setters for entities and loadResources

--- app/src/main/java/se/mau/group12/assigment3/database/Exercise.java
+++ app/src/main/java/se/mau/group12/assigment3/database/Exercise.java
@@ -16,7 +16,7 @@ public class Exercise {
     public String description;
 
     @ColumnInfo(name = "duration")
-    public String duration;
+    public int duration;
 
     @ColumnInfo(name = "difficulty")
     public int difficulty;
@@ -29,6 +29,70 @@ public class Exercise {
 
     @ColumnInfo(name = "image")
     public String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getVideo_id() {
+        return video_id;
+    }
+
+    public void setVideo_id(String video_id) {
+        this.video_id = video_id;
+    }
+
+    public int getVideo_timestamp() {
+        return video_timestamp;
+    }
+
+    public void setVideo_timestamp(int video_timestamp) {
+        this.video_timestamp = video_timestamp;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getDate_interval() {
+        return date_interval;
+    }
+
+    public void setDate_interval(int date_interval) {
+        this.date_interval = date_interval;
+    }
 
     @ColumnInfo(name = "date_interval")
     public int date_interval;
--- app/src/main/java/se/mau/group12/assigment3/database/Training.java
+++ app/src/main/java/se/mau/group12/assigment3/database/Training.java
@@ -16,7 +16,7 @@ public class Training {
     public String description;
 
     @ColumnInfo(name = "duration")
-    public String duration;
+    public int duration;
 
     @ColumnInfo(name = "difficulty")
     public int difficulty;
@@ -36,5 +36,75 @@ public class Training {
     @ColumnInfo(name = "exercise_key_4")
     public String exercise_key_4;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getTraining_image() {
+        return training_image;
+    }
+
+    public void setTraining_image(String training_image) {
+        this.training_image = training_image;
+    }
+
+    public String getExercise_key_1() {
+        return exercise_key_1;
+    }
+
+    public void setExercise_key_1(String exercise_key_1) {
+        this.exercise_key_1 = exercise_key_1;
+    }
+
+    public String getExercise_key_2() {
+        return exercise_key_2;
+    }
+
+    public void setExercise_key_2(String exercise_key_2) {
+        this.exercise_key_2 = exercise_key_2;
+    }
+
+    public String getExercise_key_3() {
+        return exercise_key_3;
+    }
+
+    public void setExercise_key_3(String exercise_key_3) {
+        this.exercise_key_3 = exercise_key_3;
+    }
+
+    public String getExercise_key_4() {
+        return exercise_key_4;
+    }
+
+    public void setExercise_key_4(String exercise_key_4) {
+        this.exercise_key_4 = exercise_key_4;
+    }
 }
